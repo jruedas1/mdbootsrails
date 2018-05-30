a9cca39bd6b57ec7b78b0a7f1c56de8e24ba03af
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-05-30 15:42:43 -0500

    fixed config asset path config application

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.paths << Rails.root.join("app", "assets", "font", "roboto")
-config.assets.paths << Rails.root.join("vendor", "assets", "img")
-
 module Mdbootstrap
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join("app", "assets", "font", "roboto")
+    config.assets.paths << Rails.root.join("vendor", "assets", "img")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
